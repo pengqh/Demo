99a65974d3ef1e5091447492b595e740182411de
pengqh <pengquanhua@yeah.net>
2017-08-16 09:40:58 +0800

    New Repository
    1、创建WorkSpace
    2、创建iMessage

---
+++ iMessage/iMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iMessage
+//
+//  Created by quanhua on 2017/8/16.
+//  Copyright © 2017年 idreamsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
